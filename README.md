21d789c21a0d42771d8868da94d09137064ed878
taozhang1029 <taozhang1029@qq.com>
2021-08-11 20:58:05 +0800

    基础测试

---
+++ README.md
@@ -0,0 +1,2 @@
+# Binlog
+## 使用 Python 监听 MySQL 的 Binlog
--- main.py
+++ main.py
@@ -1,2 +1,31 @@
+from pymysqlreplication import BinLogStreamReader
+from pymysqlreplication.row_event import (
+    WriteRowsEvent,
+    UpdateRowsEvent,
+    DeleteRowsEvent
+)
+
+MYSQL_SETTINGS = {
+    "host": "localhost",
+    "user": "root",
+    "password": "zhangtao552",
+}
+
 if __name__ == '__main__':
-    print(123)
+    reader = BinLogStreamReader(
+        connection_settings=MYSQL_SETTINGS,
+        server_id=9,
+        blocking=True,
+        # 只监听 数据库 chenxi
+        only_schemas="chenxi",
+        only_events=(WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent)
+    )
+
+    for binlog_stream in reader:
+        print()
+        for row in binlog_stream.rows:
+            print(row)
+            print(row.get("data"))
+            print("=========")
+
+    reader.close()
